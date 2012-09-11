e13475609e4879b9b56f5692844bc25cd6382e0e
Laurynas Verzukauskas <laurynas@whatever.com>
2012-09-11 21:44:37 +0300

    Added DB connection config file

---
+++ Code/config.php
@@ -0,0 +1,7 @@
+<?php
+	define("MYSQL_HOST", "localhost");
+	define("MYSQL_USER", "minimalprice");
+	define("MYSQL_PASS", "password");
+	define("MYSQL_NAME", "minimalprice");
+	define("MYSQL_PREFIX", "mp_");
+?>
