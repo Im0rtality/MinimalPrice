0dfebbcddcbe4e1fda2f51fd041e2e750204ab97
Laurynas Verzukauskas <laurynas@whatever.com>
2012-09-14 19:30:44 +0300

    Page parsing tests

--- Code/Classes/class.shopparser.php
+++ Code/Classes/class.shopparser.php
@@ -2,15 +2,41 @@
 	class ShopParser{
 		protected $Hostname;	
 		
-		public $LastError;				
+		public  $LastError;				
 		private $DBLink;
+		private $Reader;
+		private $Buffer;
+		private $DOM;
+		private $Nodes;
 		
-		function ShopParser($DB){
+		
+		function ShopParser($DB, $R){
 			$this->DBLink = $DB;
+			$this->Reader = $R;
+			$this->DOM = new DOMDocument();
 		}
 		
-		function GetContents($PageUrl) {
-			
+		function GetContent($PageUrl) {
+			$this->Buffer = $this->Reader->Get($PageUrl);
+			return $this->Buffer;
+		}
+		
+		function Load() {
+			if (!empty($this->Buffer)) {
+				$this->DOM->loadHTML($Buffer);
+				return true;
+			} else {
+				$this->LastError = "Error: Buffer is empty";
+				return false;
+			}
+		}
+		
+		function ExtractData() {
+			$this->Nodes = $this->DOM->getElementsByTagName('table');
+		}
+		
+		function GetOutput() {
+			return $this->Nodes;
 		}
 	}
 ?>
--- Code/ajax/parser.php
+++ Code/ajax/parser.php
@@ -1,4 +1,16 @@
-<?php
+﻿<?php
+	include("../config.php");
+	include("../Classes/class.mysql.php");
+	include("../Classes/class.remotereader.php");
+	include("../Classes/class.shopparser.php");
 	
+	$db = new MySql();
+	$reader = new RemoteReader();
+	$reader->SetUserAgent("Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0");
+	$parser = new ShopParser($db, $reader);
 	
+	$parser->GetContent($_GET['url']);
+	$parser->ExtractData();
+	
+	echo '<pre>' . print_r($parser->GetOutput(), true) . '</pre>';
 ?>
