5dfb2730f3830eabc95eb7e96b8315b3cf9a9c66
Laurynas Verzukauskas <laurynas@whatever.com>
2012-09-26 13:26:29 +0300

    innerHTML() now can print current element as well

--- Code/Shared/utils.php
+++ Code/Shared/utils.php
@@ -3,34 +3,49 @@
 		echo "<pre>" . print_r($data, true) . "</pre>";
 	}
 	
-	function innerHTML( $contentdiv ) {
+	function innerHTML( $contentdiv, $self = false ) {
 		$r = '';
-		$elements = $contentdiv->childNodes;
-		foreach( $elements as $element ) { 
-			if ( $element->nodeType == XML_TEXT_NODE ) {
-				$text = $element->nodeValue;
-				// IIRC the next line was for working around a
-				// WordPress bug
-				//$text = str_replace( '<', '&lt;', $text );
-				$r .= $text;
+		if ($self) {
+			$r .= '<';
+			$r .= $contentdiv->nodeName;
+			if ( $contentdiv->hasAttributes() ) { 
+				$attributes = $contentdiv->attributes;
+				foreach ( $attributes as $attribute )
+					$r .= " {$attribute->nodeName}='{$attribute->nodeValue}'" ;
 			}	 
-			// FIXME we should return comments as well
-			elseif ( $element->nodeType == XML_COMMENT_NODE ) {
-				$r .= '';
-			}	 
-			else {
-				$r .= '<';
-				$r .= $element->nodeName;
-				if ( $element->hasAttributes() ) { 
-					$attributes = $element->attributes;
-					foreach ( $attributes as $attribute )
-						$r .= " {$attribute->nodeName}='{$attribute->nodeValue}'" ;
+			$r .= '>';
+		}
+					$elements = $contentdiv->childNodes;
+		if (!empty($elements)) {
+			foreach( $elements as $element ) { 
+				if ( $element->nodeType == XML_TEXT_NODE ) {
+					$text = $element->nodeValue;
+					// IIRC the next line was for working around a
+					// WordPress bug
+					//$text = str_replace( '<', '&lt;', $text );
+					$r .= $text;
 				}	 
-				$r .= '>';
-				$r .= innerHTML( $element );
-				$r .= "</{$element->nodeName}>";
+				// FIXME we should return comments as well
+				elseif ( $element->nodeType == XML_COMMENT_NODE ) {
+					$r .= '';
+				}	 
+				else {
+					$r .= '<';
+					$r .= $element->nodeName;
+					if ( $element->hasAttributes() ) { 
+						$attributes = $element->attributes;
+						foreach ( $attributes as $attribute )
+							$r .= " {$attribute->nodeName}='{$attribute->nodeValue}'" ;
+					}	 
+					$r .= '>';
+					$r .= innerHTML( $element );
+					$r .= "</{$element->nodeName}>";
+				}	 
 			}	 
-		}	 
+		}
+		if ($self) {
+			$r .= "</{$contentdiv->nodeName}>";
+		}
 		return $r;
 	}
 ?>
