66352f5401ca9c6e8597be2563a65a5594ccca3e
Laurynas Verzukauskas <laurynas@whatever.com>
2012-09-27 14:16:13 +0300

    Template Manager cleanup & additions
    Added another temporary hardcode of templates till we can use DB for
    that

--- Code/Classes/class.template-manager.php
+++ Code/Classes/class.template-manager.php
@@ -8,16 +8,18 @@
 		}
 		
 		function Load($ID) {
-			debug("TemplateManager->Load('$ID')");
+//			debug("TemplateManager->Load('$ID')");
 			// load $this->Template from DB
 			// for now lets hardcode data for skytech.lt
 			$classname = "productListing";
 			
-			$this->Template['product-list'] = "//table[contains(@class,\"$classname\")][1]//tr";
+			$this->Template['product-list'] = "//table[contains(@class,\"productListing\")][1]//tr";
+			$this->Template['product-list-skip'] = 1;
+			$this->Template['product-list-item'] = "<template><td>{serial}</td><td></td><td><a href='{href}'></a> </td><td></td><td>{price}</td></template>";
 		}
 		
 		function GetItem($Piece) {
-			debug("TemplateManager->GetItem('$Piece')");
+//			debug("TemplateManager->GetItem('$Piece')");
 			if (!empty($this->Template[$Piece])) {
 				return $this->Template[$Piece];
 			}			
