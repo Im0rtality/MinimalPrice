422dd5dec972a3a2c35ee1e536606272d85f390a
Laurynas Verzukauskas <laurynas@whatever.com>
2012-10-08 15:21:09 +0300

    Added Parser Templates page

--- Code/Admin/Pages/page-parser-template.php
+++ Code/Admin/Pages/page-parser-template.php
@@ -18,7 +18,43 @@
 			$DB = MySql::getInstance();
 			$DB->ExecuteSQL($query);
 			$Data = $DB->GetRecordSet();
-			$code .= Formatter::DbDataToHtmlTable($Data);
+			//$code .= Formatter::DbDataToHtmlTable($Data);
+			$code .= <<<EOT
+<table class="table table-condensed table-hover table-stripped">
+<tr>
+	<th width="20px">ID</th>
+	<th>Name</th>
+	<th width="20px">&nbsp;</th>
+	<th width="20px">&nbsp;</th>
+</tr>
+EOT;
+			foreach ($Data as $Row) {
+				$code .= <<<EOT
+<tr>
+	<td>{$Row['id']}</td>
+	<td>{$Row['name']}</td>
+	<td><a class='btn btn-mini' href='#' data-id='{$Row['id']}'><i class='icon-pencil'></i></a></td>
+	<td><a class='btn btn-mini btn-danger' href='#' data-id='{$Row['id']}'><i class='icon-remove-sign'></i></a></td>
+</tr>
+EOT;
+
+			/*	$object = json_decode($Row['value'], true);
+				$xpath = htmlentities($object['product-list']);
+				$regex = htmlentities($object['product-list-item']);
+				//debug(htmlentities($object['product-list-item']));
+				$code .= <<< EOT
+<form method="GET" action="" class="form-horizontal">
+	<legend>{$Row['name']}</legend>
+	<input value="{$xpath}" class="input-xlarge" name="product-list" id="product-list" type="text">
+	<input value="{$regex}" class="input-xlarge" name="product-list-item" id="product-list" type="text">
+	<input value="{$object['product-list-skip']}" class="input-mini" name="product-list-skip" id="product-list-skip" type="text">
+	<input value="{$object['product-list-field-count']}" class="input-mini" name="product-list-field-count" id="product-list-field-count" type="text">
+	<button type="button" class="btn">More</button>
+	<button type="submit" class="btn btn-primary">Save</button>
+</form>
+EOT;*/
+			}
+			$code .= "</table>";
 
 			return $code;
 		}
