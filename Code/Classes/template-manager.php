675937c194bef4302ec67b3d95c1e3ed7161b1ab
Laurynas Verzukauskas <laurynas@whatever.com>
2012-10-12 11:42:13 +0300

    TemplateManager now takes data from DB
    Data was hardcoded before

--- Code/Classes/template-manager.php
+++ Code/Classes/template-manager.php
@@ -1,21 +1,30 @@
 <?php
+	require_once(dirname(__FILE__) . "/mysql.php");
+
 	class TemplateManager{
 		private $Template;
+		private $Name;
 		private $DB;
 		
-		function _construct($DB){
-			$this->DB = $DB;
+		function _construct(){
+			$this->DB = MySql::getInstance();
 		}
 		
 		function Load($ID) {
 			// load $this->Template from DB
 			// for now lets hardcode data for skytech.lt
-			$classname = "productListing";
-			
-			$this->Template['product-list'] = "//table[contains(@class,\"productListing\")][1]//tr";
-			$this->Template['product-list-skip'] = 1;
-			$this->Template['product-list-field-count'] = 3;
-			$this->Template['product-list-item'] = "<template><td>{serial}</td><td></td><td><a href='{href}'></a> </td><td></td><td>{price}</td></template>";
+			$this->DB->ExecuteSQL("SELECT * FROM parser_template WHERE (id={$ID}) LIMIT 1");			
+			$Data = $this->DB->GetRecordSet()[0];
+			$this->Name = $Data['name'];
+			$this->Template = json_decode($Data['value'], true);
+
+			/*
+				$this->Template['product-list'] = "//table[contains(@class,\"productListing\")][1]//tr";
+				$this->Template['product-list-skip'] = 1;
+				$this->Template['product-list-field-count'] = 3;
+				$this->Template['product-list-item'] = "<template><td>{serial}</td><td></td><td><a href='{href}'></a> </td><td></td><td>{price}</td></template>";
+			*/
+			debug($this->Template);
 		}
 		
 		function GetItem($Piece) {
