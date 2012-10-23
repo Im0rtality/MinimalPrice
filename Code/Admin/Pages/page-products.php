d5e4f02b837f2f02df8b64a489159564e361b053
Marchello700 <mantas.taroza@gmail.com>
2012-10-24 00:41:34 +0300

    Pridetas prekiu rodymo karkasas, reikia suderinti vieta, galbut koda naudoti net ne php, o javascript pagalba su jquery, reikia gauti funkcijas su sql uzklausomis duomenu gavimui/updeitinimui/trinimui

--- Code/Admin/Pages/page-products.php
+++ Code/Admin/Pages/page-products.php
@@ -8,13 +8,41 @@
 			$this->options['link'] = 'products';
 			$this->options['name'] = 'Products';
 			parent::__construct();
+
 		}
 
+        public function set_products(){
+            //uzklausa i sql ir duomenu paemimas array formatu bei array ilgiu paemimas (stuff)
+            $shop = array( array("rose", 1.25 , 15),
+                array("daisy", 0.75 , 25),
+                array("orchid", 1.15 , 7),
+                array("stuff", 1.15 , 7),
+                array("more_stuff", 1.15 , 7),
+                array("additional_stuff", 1.15 , 7),
+                array("even_more_stuff", 1.15 , 7),
+                array("no_more_stuff", 1.15 , 7)
+            );
+            $size1 = 8;
+            $size2 = 3;
+            for ($i = 0; $i < $size1; $i++ )
+            {
+                echo '<form>';
+                for ($i2 = 0; $i2 < $size2; $i2++ )
+                    echo '<input type="text" id="nameField" value='.$shop[$i][$i2].' length=50 onkeypress="updateName(this.value)"/>';
+                echo '<button type="Submit">Update</button>';
+                echo '<button type="Submit">Delete</button>';
+                echo '</form> ';
+            }
+        }
+
 		public function generate() {
+            $this->set_products();
 			$code = "<h1>{$this->options['name']}</h1>";
 
 			return $code;
 		}
+
+
 	}
 
 	new PageProducts();
