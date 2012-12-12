2f30f6fa024ea4bd5fda005ad253335f472f16b4
Laurynas Verzukauskas <laurynas@whatever.com>
2012-12-12 21:32:34 +0200

    [Admin panel] Added "Shop Parser Editor"

---
+++ Code/Admin/Pages/page-edit-parser-template.php
@@ -0,0 +1,73 @@
+<?php
+	require_once(dirname(__FILE__) . "\..\..\Classes\mysql.php");
+	require_once(dirname(__FILE__) . '\..\..\Classes\formatter.php');
+	require_once(dirname(__FILE__) . '\..\interface.page-generator.php');
+
+	class PageEditShopParser extends PageModule implements PageGenerator{
+		function __construct(){
+			$this->options['link'] = 'editshopparser';
+			$this->options['name'] = 'Shop Parser Editor';
+			$this->options['inSidebar'] = false;
+			parent::__construct();
+			switch ($this->action) {
+				case 'edit':
+					break;
+				case 'save':
+					break;
+				case 'delete':
+					break;
+			}
+			$this->table = 'parser_template';
+		}
+
+		public function generate() {
+			$code = "";
+			switch ($this->action){
+				case 'save':
+					$code .= Formatter::ArraySimpleDump2($_POST, "POST Data");
+					$query = Formatter::QuerySaveEditor($_POST, $this->table);
+
+					
+					$DB = MySql::getInstance();
+					$DB->ExecuteSQL($query);
+
+					$code .= Formatter::Redirect('shopparsers', 3000, "Redirecting to list in 3 seconds.");
+					break;
+				case 'edit':
+					$query = Formatter::QueryLoadEditor($this->table, $_GET["id"]);
+
+					
+					$DB = MySql::getInstance();
+					$DB->ExecuteSQL($query);
+					$Data = $DB->GetRecordSet();
+
+					$code .= Formatter::ArraySimpleDump2($Data[0], "<i>$query</i>");
+
+					$FormData['id'] = $Data[0]["id"];
+					$FormData['page'] = "editshopparser";
+					$FormData['fields'][] = ["label" => "ID",
+											 "type" => "text", 
+											 "id" => "", 
+											 "value" => $Data[0]["id"], 
+											 "disabled" => true];
+					
+					$FormData['fields'][] = ["label" => "Name",
+											 "type" => "text", 
+											 "id" => "name", 
+											 "value" => $Data[0]["name"]];
+
+					$FormData['fields'][] = ["label" => "Value",
+											 "type" => "textarea", 
+											 "id" => "value", 
+											 "value" => $Data[0]["value"]];
+
+					$code .= Formatter::Form($FormData, NULL);
+				break;
+
+			}
+			return $code;
+		}
+	}
+
+	new PageEditShopParser();
+?>
