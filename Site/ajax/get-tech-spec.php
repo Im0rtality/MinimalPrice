fa8d4e158b37a681c4fce32efc23e349809ea3e7
Dovydas <dovydas.cerkus@gmail.com>
2012-12-14 21:01:35 +0200

    commit test

--- Site/ajax/get-tech-spec.php
+++ Site/ajax/get-tech-spec.php
@@ -3,7 +3,7 @@
 	require_once(dirname(__FILE__) . "/../../Code/config.php");
 	require_once(dirname(__FILE__) . "/../../Code/Classes/remote-reader.php");
 	require_once(dirname(__FILE__) . "/../../Code/Parsers/parser.cpuworld.php");
-
+	require_once(dirname(__FILE__) . "/../../Code/Classes/Mappers/parsed-cpu-mapper.php");
 
 	if ((isset($_GET['sn'])) && (!empty($_GET['sn']))) {
 		$reader = new RemoteReader();
@@ -11,9 +11,10 @@
 		$parser = new CpuWorldParser($reader);
 		
 		
-		$data = $parser->SearchFor($_GET['sn']);
-		$data->store();
-		debug($data);
+		$spec = $parser->SearchFor($_GET['sn']);
+		debug($spec);
+		$mapper = new ParsedCPUMapper();
+		$mapper->store( $spec->get());
 	} else {
 		die("Cannot parse: sn paremeter is empty or not set");
 	}
