39874439486f3f750a4bb0e42c1147bab8d77fe7
Dovydas <dovydas.cerkus@gmail.com>
2012-12-14 19:21:02 +0200

    cpu-spec.php, deleted my method store().
    Back to original Laurynas code

--- Code/Classes/cpu-spec.php
+++ Code/Classes/cpu-spec.php
@@ -25,9 +25,6 @@
 			return $value;
 		}		
 		
-		/**
-		 * adds specifications one key, one value at time
-		 */
 		public function add($key, $value) {
 			$key = $this->translateKey($key);
 			if (!in_array($key, self::$IGNORED_FIELDS)) {
@@ -37,46 +34,6 @@
 		
 		public function get() {
 			return $this->data;
-		}
-		
-		/**
-		 * stores data to DB
-		 */
-		public function store() {
-			$db = DB::getInstance();
-			
-			$product = R::dispense('product');
-			// ----- add product data -----
-			$product->series = $this->data['Family'];
-			$product->model = $this->data['Model number'];
-			$product->code = $this->data['CPU part numbers'];
-			$product->description = "a SUPER unique fcuk yea preprocessor";
-			
-			$product->category_id = 1;
-			// -----/add product data -----
-			
-			
-			
-			// add existing manufacturer
-			$manuf = $this->data['Family'];
-			$manufacturer = R::findOne('manufacturer', ' name = ?', array($manuf));
-							 										
-			$manufacturer->ownProduct[] = $product; // create one to many relation, adding a bean
-			
-			/*
-			// add existing category
-			$category = R::findOne('category', 'category = Procesoriai');
-			$category->ownProduct[] = $product;	// one to many relation, adding a bean
-		
-			// add new product image
-			$productImage = R::dispense('pimage');
-			$productImage->url = 'dummyImageURL' . rand() . '.jpg'; // create random image address
-			R::store($productImage);
-			
-			$product->sharedPimage[] = $productImage; // many to many relation, adding a bean
-			*/
-
-			R::store($product); 
 		}
 	}
 ?>
