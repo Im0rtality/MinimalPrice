843e3d023e709cf002284f7a8a733a33a4124e6b
Laurynas Verzukauskas <laurynas@whatever.com>
2012-12-16 17:22:02 +0200

    Restored to last known working state

--- Site/ajax/get-tech-spec.php
+++ Site/ajax/get-tech-spec.php
@@ -1,20 +1,16 @@
-<?php
-       	require_once(dirname(__FILE__) . "/../../Code/Shared/utils.php");
+﻿<?php
+	require_once(dirname(__FILE__) . "/../../Code/Shared/utils.php");
 	require_once(dirname(__FILE__) . "/../../Code/config.php");
 	require_once(dirname(__FILE__) . "/../../Code/Classes/remote-reader.php");
 	require_once(dirname(__FILE__) . "/../../Code/Parsers/parser.cpuworld.php");
-	require_once(dirname(__FILE__) . "/../../Code/Classes/Mappers/parsed-cpu-mapper.php");
+
 
 	if ((isset($_GET['sn'])) && (!empty($_GET['sn']))) {
 		$reader = new RemoteReader();
 
 		$parser = new CpuWorldParser($reader);
 		
-		
-		$spec = $parser->SearchFor($_GET['sn']);
-		debug($spec);
-		$mapper = new ParsedCPUMapper();
-		$mapper->store( $spec->get());
+		debug($parser->SearchFor($_GET['sn']));
 	} else {
 		die("Cannot parse: sn paremeter is empty or not set");
 	}
