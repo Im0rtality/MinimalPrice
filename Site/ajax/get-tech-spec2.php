f5e6841163c106681964bb39fd530bbe2fef8680
Dovydas <dovydas.cerkus@gmail.com>
2012-12-18 00:54:21 +0200

    [DB update] (see descr.) merchandise and cpu
    merchandise.product_id NOT NULL ( was NULLABLE)
    theese cpu fields set NULLABLE (was not null):
    turbo_frequency_mhz
    power_consumption_w
    launch_date
    bus_core_ratio
    bus_speed_mhz
    max_tdp
    max_ram_mb
    ram_channels
    ram_bandwidth
    max_cpus_configuration

---
+++ Site/ajax/get-tech-spec2.php
@@ -0,0 +1,23 @@
+﻿<?php
+	require_once(dirname(__FILE__) . "/../../Code/Shared/utils.php");
+	require_once(dirname(__FILE__) . "/../../Code/config.php");
+	require_once(dirname(__FILE__) . "/../../Code/Classes/remote-reader.php");
+	require_once(dirname(__FILE__) . "/../../Code/Parsers/parser.cpuworld.php");
+    require_once(dirname(__FILE__) . "/../../Code/Classes/Mappers/parsed-cpu-mapper.php");
+
+
+	if ((isset($_GET['sn'])) && (!empty($_GET['sn']))) {
+		$reader = new RemoteReader();
+
+		$parser = new CpuWorldParser($reader);
+		
+        $spec = $parser->SearchFor($_GET['sn']);
+		debug($spec);
+        
+        $mapper = new ParsedCPUMapper();
+        $mapper->store($spec->get());
+	} else {
+		die("Cannot parse: sn paremeter is empty or not set");
+	}
+	
+?>
