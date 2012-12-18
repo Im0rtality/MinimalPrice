422826026e457f514a4e4b452ac33709403bf643
Dovydas <dovydas.cerkus@gmail.com>
2012-12-18 03:43:59 +0200

    added merchandise saver and mapper. TODO let update data, not just add

--- Code/Classes/Savers/parsed-merchandise-mapper.php
+++ Code/Classes/Savers/parsed-merchandise-mapper.php
@@ -1,97 +1,52 @@
-﻿<?php
-    require_once(dirname(__FILE__) . "/../Database/db.php");
-    require_once(dirname(__FILE__) . "/../../Shared/utils.php");
-
+<?php
     class ParsedMerchandiseMapperException
     {
         // exceptions
         const CANT_MAP_CURRENCY = 101;
         const CANT_MAP_COST = 102;
-        const CURRENCY_UNKNOWN = 103;
-        const PRODUCT_UNKNOWN = 104;
-        
     }
-    
-    class ParsedMerchandiseMapper
-    {	
-        private $db;
-        private $merchandise;
-        private $shop;
-        private $currency;
-        private $product;
-        
-        
-        
-        private function splitCostCurrency($costCurrency)
-        {
-            if (preg_match("#[0-9]*[.,][0-9]*#", $costCurrency, $matches) > 0) {
-                $cost = $matches[0];
-                if (preg_match("/([A-Z][a-z]*)/", $costCurrency, $costMatches) > 0) {
-                    $currency = substr($costMatches[0], 0, 3); // gets first 3 letters
-                }
-                else {
-                    throw new Exception('Merchandise-mapper: Cannot map parsed currency', ParsedMerchandiseMapperException::CURRENCY_UNKNOWN);
-                }
-            }
-            else {
-                throw new Exception('Merchandise-mapper: Cannot map parsed cost', ParsedMerchandiseMapperException::COST_UNKNOWN);
-            }   
 
-            return array($cost, $currency);
-        }
-        
-        
-        
+    class ParsedMerchandiseMapper
+    {
         /**
          * Maps parser keys and data to database tables' fields
          * @param associative array $parsedData data from parser
          * return associative array keys - tables' fields, values - parsed data
          */
-        private function map($parsedData)
+        public static function map($parsedData)
         {
-            list($cost, $currency) = $this->splitCostCurrency($parsedData['price']);
+            $fields['serial'] = preg_replace("/[^a-zA-Z0-9]+/", "", $parsedData['serial']);
             
-            // merchandise fields
-            $fields = [
-                "serial" => $parsedData['serial'],
-                "currency" => $currency,
-                "cost" => $cost,
-                "amount" => 1, 
-                //"warranty_mm" => 
-                "url" => $parsedData['href'],
-            ];
+            list($cost, $currency) = self::splitCostCurrency($parsedData['price']);
+            if ( !empty($cost)) {
+                $fields['cost'] = $cost;
+            }
+            else {
+                throw new Exception(__CLASS__ . ' Cannot map parsed cost', ParsedMerchandiseSaverException::CANT_MAP_COST);
+            }
+            if ( !empty($currency)) {
+                $fields['currency'] = $currency;
+            }
+            else {
+                throw new Exception(__CLASS__ . ' Cannot map parsed currency', ParsedMerchandiseSaverException::CANT_MAP_CURRENCY);
+            }
+            $fields['amount'] = 1; // TODO
+            //$fields['warranty_mm'] =  // TODO
+            $fields['url'] = $parsedData['href'];
             
             return $fields;
         }
         
-        public function store($shopId, $parsedData)
+        
+        private static function splitCostCurrency($costCurrency)
         {
-            $this->db = DB::getInstance();
-            
-            $fields = $this->map($parsedData);
-            
-            // search for given product
-            $this->product = R::findOne('product', ' code = ?', array( $fields['serial']));
-            if ( !empty($this->product)) {
-                $this->merchandise = R::dispense('merchandise');
-                $this->shop = R::load('shop', $shopId); // get given shop
-                $this->currency = R::findOne('currency', ' name LIKE ?', array( $fields['currency']));
-                if ( !empty($this->currency)) {
-
-                    // relations
-                    $this->merchandise->shop = $this->shop;
-                    $this->merchandise->currency = $this->currency;
-                    $this->merchandise->product = $this->product;
-
-                    R::store($this->merchandise);
+            if (preg_match("#[0-9]*[.,][0-9]*#", $costCurrency, $matches) > 0) {
+                $cost = $matches[0];
+                if (preg_match("/([A-Z][a-z]*)/", $costCurrency, $costMatches) > 0) {
+                    $currency = strtoupper( substr($costMatches[0], 0, 3)); // gets first 3 letters
                 }
-                else {
-                    throw new Exception('Merchandise-mapper: Currency not found in Database', ParsedMerchandiseMapperException::CURRENCY_UNKNOWN);
-                }
-            } 
-            else {
-                throw new Exception('Merchandise-mapper: Product with such serial not exists', ParsedMerchandiseMapperException::PRODUCT_UNKNOWN);
             }
+            return array($cost, $currency);
         }
     }
 ?>
--- Code/Classes/Savers/parsed-merchandise-products-saver.php
+++ Code/Classes/Savers/parsed-merchandise-products-saver.php
@@ -1,59 +1,46 @@
 <?php
-	require_once(dirname(__FILE__) . "/../../Code/Shared/utils.php");
-	require_once(dirname(__FILE__) . "/../../Code/config.php");
-	require_once(dirname(__FILE__) . "/../../Code/Classes/remote-reader.php");
-	require_once(dirname(__FILE__) . "/../../Code/Parsers/parser.simple-shop-parser.php");
-    require_once(dirname(__FILE__) . "/../../Code/Classes/Mappers/parsed-merchandise-mapper.php");
-
-	function checkParameter($key) {
-		if ((!isset($_GET[$key])) || (empty($_GET[$key]))) {
-			die("Cannot parse: `{$key}` paremeter is empty or not set");
-		}
-	}
-	
-	// check if any parameter is missing
-	checkParameter('url');
-	checkParameter('shop');
-
-	$parser = new SimpleShopParser($_GET['shop']);
-	
-	$parser->parsePage($_GET['url']);
-    $merchandise = $parser->GetData();
-	debug($merchandise);
+	require_once(dirname(__FILE__) . "/parsed-merchandise-saver.php");
+    require_once(dirname(__FILE__) . "/../../../Site/ajax/get-tech-spec2.php");
     
-    $shopId = 1; // TODO get merchandise parsers' given shop id. 
-    storeAll($shopId, $merchandise);
     
-    /**
-     * stores all parsed $merchandise, links given shopId. 
-     * If there is no product with given serial, tries to get specification for it and store in database.
-     * If parsed data is not correct gives echos Exceptions.
-     * @param type $shopId
-     * @param type $merchandise
-     */
-    function storeAll($shopId, $merchandise) {
-        $merchandiseMapper = new ParsedMerchandiseMapper();
-        $merchandiseNo = 0;
-        $nTriesToCreateNew = 0; // number of tries to create new product if it is not found, 
+    
+    class ParsedMPS { // parsed merchandise product saver
         
-        while ($merchandiseNo < count($merchandise)) {
-            try {
-                $merchandiseMapper->store($shopId, $merchandise[$merchandiseNo]);
-                $nTriesToCreateNew = 0;
-            }
-            catch (Exception $e){
-                if ($e->getCode() == ParsedMerchandiseMapperException::PRODUCT_UNKNOWN) {
-                    echo 'TODO add new product <br>';           // TODO get specs and create a new product
-                    $nTriesToCreateNew++;
+        /**
+         * stores all parsed $merchandise, links given shopId. 
+         * If there is no product with given serial, tries to get specification for it and store in database.
+         * If parsed data is not correct echos Exceptions.
+         * @param type $shopId
+         * @param type $merchandise
+         */
+        function storeAll($shopId, $merchandise) {
+            $merchandiseSaver = new ParsedMerchandiseSaver();
+            $specGetter = new GetTechSpec();
+            $i = 0;
+            $hasTriedCreate = false; // has tried to create product
+
+            while ($i < count($merchandise)) {
+                try {
+                    echo 'preke nr = ', $i, ' bandoma saugoti<br />';
+                    $merchandiseSaver->store($shopId, $merchandise[$i]);
+                    echo 'preke nr = ', $i, ' sekmingai issaugota<br />';
+                    $i++;
+                    $hasTriedCreate = false;
                 }
-                else {
-                    echo $e, '\n'; // print error
+                catch (Exception $e){
+                    if ( !$hasTriedCreate && $e->getCode() == ParsedMerchandiseSaverException::PRODUCT_UNKNOWN) {
+                        echo 'prekes nr = ', $i, ' atitinkancio produkto nera, ieskom tokio produkto<br />';
+                        $sn = preg_replace("/[^a-zA-Z0-9]+/", "", $merchandise[$i]['serial']);
+                        $specGetter->parseAndStore($sn);
+                        $hasTriedCreate = true;
+                    }
+                    else {
+                        echo 'Error: ', $e->getMessage(), '<br />';
+                        $i++;
+                        $hasTriedCreate = false;
+                    }
                 }
             }
-            
-            if ($nTriesToCreateNew > 1) {
-                $merchandiseNo++;
-            } // else work again with same shop item;
         }
     }
 ?>
