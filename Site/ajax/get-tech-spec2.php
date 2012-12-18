e67753ec6d694f7c8ab53fe1539eec75afae35f4
Dovydas <dovydas.cerkus@gmail.com>
2012-12-18 03:41:21 +0200

    added a modified copy of get-tech-spec-php

--- Site/ajax/get-tech-spec2.php
+++ Site/ajax/get-tech-spec2.php
@@ -1,23 +1,35 @@
-﻿<?php
+<?php
 	require_once(dirname(__FILE__) . "/../../Code/Shared/utils.php");
 	require_once(dirname(__FILE__) . "/../../Code/config.php");
 	require_once(dirname(__FILE__) . "/../../Code/Classes/remote-reader.php");
 	require_once(dirname(__FILE__) . "/../../Code/Parsers/parser.cpuworld.php");
-    require_once(dirname(__FILE__) . "/../../Code/Classes/Mappers/parsed-cpu-mapper.php");
+    require_once(dirname(__FILE__) . "/../../Code/Classes/Savers/parsed-cpu-saver.php");
 
+    class GetTechSpec 
+    {
+        function parseAndStore($sn) {
+            if ((isset($sn)) && (!empty($sn))) {
+                $reader = new RemoteReader();
 
-	if ((isset($_GET['sn'])) && (!empty($_GET['sn']))) {
-		$reader = new RemoteReader();
+                $parser = new CpuWorldParser($reader);
 
-		$parser = new CpuWorldParser($reader);
-		
-        $spec = $parser->SearchFor($_GET['sn']);
-		debug($spec);
-        
-        $mapper = new ParsedCPUMapper();
-        $mapper->store($spec->get());
-	} else {
-		die("Cannot parse: sn paremeter is empty or not set");
-	}
-	
+                $spec = $parser->SearchFor($sn);
+                debug($spec);
+                
+                $specArray = $spec->get();
+                if ( !empty($specArray)) {
+                    $saver = new ParsedCPUSaver();
+                    try {
+                        $model = $saver->store($sn, $specArray);
+                        echo '<h2><strong> SAVED TO DB: Model = ', $model, ' Serial = ', $sn, '</strong></h2><br />';
+                    }
+                    catch (Exception $e) {
+                        echo 'Error', $e, '<br />';
+                    }
+                } 
+           } else {
+                die("Cannot parse: sn paremeter is empty or not set");
+           }
+        }
+    }
 ?>
