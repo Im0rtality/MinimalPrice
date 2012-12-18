570d034e293017d58842ed86e5826db967c71fa5
Dovydas <dovydas.cerkus@gmail.com>
2012-12-18 11:12:35 +0200

    Revert "[Spec parser] Restored working version"
    This reverts commit fc156a7f667644caa184b69c112dbc3f17447abb.

--- Site/ajax/get-tech-spec.php
+++ Site/ajax/get-tech-spec.php
@@ -3,6 +3,7 @@
 	require_once(dirname(__FILE__) . "/../../Code/config.php");
 	require_once(dirname(__FILE__) . "/../../Code/Classes/remote-reader.php");
 	require_once(dirname(__FILE__) . "/../../Code/Parsers/parser.cpuworld.php");
+    require_once(dirname(__FILE__) . "/../../Code/Classes/Mappers/parsed-cpu-saver.php");
 
 
 	if ((isset($_GET['sn'])) && (!empty($_GET['sn']))) {
@@ -12,7 +13,11 @@
 		
         $spec = $parser->SearchFor($_GET['sn']);
 		debug($spec);
-   	} else {
+        
+        $mapper = new ParsedCPUSaver();
+        $model = $mapper->store($_GET['sn'], $spec->get());
+        echo '<h2><strong> SAVED TO DB: Model = ', $model, ' Serial = ', $sn, '</strong></h2><br />';
+	} else {
 		die("Cannot parse: sn paremeter is empty or not set");
 	}
 	
