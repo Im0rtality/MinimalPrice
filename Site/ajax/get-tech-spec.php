29bc4c1b63a7c55f276eca9c98c0d66f294bc5bb
Dovydas <dovydas.cerkus@gmail.com>
2012-12-18 03:40:25 +0200

    get-tech-spec.php updated according to parsed-cpu-saver.php

--- Site/ajax/get-tech-spec.php
+++ Site/ajax/get-tech-spec.php
@@ -1,9 +1,9 @@
-﻿<?php
+<?php
 	require_once(dirname(__FILE__) . "/../../Code/Shared/utils.php");
 	require_once(dirname(__FILE__) . "/../../Code/config.php");
 	require_once(dirname(__FILE__) . "/../../Code/Classes/remote-reader.php");
 	require_once(dirname(__FILE__) . "/../../Code/Parsers/parser.cpuworld.php");
-    require_once(dirname(__FILE__) . "/../../Code/Classes/Mappers/parsed-cpu-mapper.php");
+    require_once(dirname(__FILE__) . "/../../Code/Classes/Mappers/parsed-cpu-saver.php");
 
 
 	if ((isset($_GET['sn'])) && (!empty($_GET['sn']))) {
@@ -14,8 +14,9 @@
         $spec = $parser->SearchFor($_GET['sn']);
 		debug($spec);
         
-        $mapper = new ParsedCPUMapper();
-        $mapper->store($spec->get());
+        $mapper = new ParsedCPUSaver();
+        $model = $mapper->store($_GET['sn'], $spec->get());
+        echo '<h2><strong> SAVED TO DB: Model = ', $model, ' Serial = ', $sn, '</strong></h2><br />';
 	} else {
 		die("Cannot parse: sn paremeter is empty or not set");
 	}
