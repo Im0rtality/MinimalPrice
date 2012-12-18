594fae72eb864cf20f8da7edeb13d8f83768a3bb
Laurynas Verzukauskas <laurynas@whatever.com>
2012-12-18 11:53:53 +0200

    [Admin panel] Show Merchandise

--- Code/Admin/Pages/page-products.php
+++ Code/Admin/Pages/page-products.php
@@ -6,13 +6,26 @@
 	class PageProducts extends PageModule implements PageGenerator{
 		function __construct(){
 			$this->options['link'] = 'products';
-			$this->options['name'] = 'Products';
+			$this->options['name'] = 'Merchandise';
 			parent::__construct();
 		}
 
 		public function generate() {
-			$code = "";
+			$query = "SELECT merchandise.id, merchandise.url, merchandise.cost, currency.code FROM `merchandise`, `currency` WHERE (merchandise.currency_id = currency.id)";
 
+			$code = "";
+			
+			$DB = MySql::getInstance();
+			$DB->ExecuteSQL($query);
+			$Data = $DB->GetRecordSet();
+
+			$settings['column_names'] = ["URL", "Price", "Currency"];
+			$settings['column_widths'] = ["", "20px", "20px"];
+			$settings['column_hidden'] = [true, false, false, false];
+			$settings['id_col'] = "id";
+			$settings['page'] = "edit{$this->options['link']}";
+			
+			$code .= CodegenTable::Table($Data, $settings);
 			return $code;
 		}
 	}
